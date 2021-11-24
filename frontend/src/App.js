de50dc0c4d959fb3e6570d22bd801370c0bc268c
tapia1013 <manbearpig1013@gmail.com>
2021-11-24 15:26:52 -0800

    react router routing and reactrouterbootstrap routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 
@@ -14,6 +15,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+
+
+
+
+const ProductScreen = () => {
+  return (
+    <div>
+      product
+    </div>
+  )
+}
+
+export default ProductScreen
