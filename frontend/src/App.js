56366e6583f7f48ea7026478127709c9212dbad9
tapia1013 <manbearpig1013@gmail.com>
2021-11-24 15:09:06 -0800

    downgraded react router dom version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,16 +9,15 @@ import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <>
-
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
