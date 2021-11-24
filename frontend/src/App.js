36b78902a566408ff7fb0a66f071ef235fb6dedf
tapia1013 <manbearpig1013@gmail.com>
2021-11-24 13:54:58 -0800

    Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
+
 
 const App = () => {
   return (
@@ -10,7 +13,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>LOL PROSHOP</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
