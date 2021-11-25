c1e14b08b88d4379621365bacfc3311518824d5f
tapia1013 <manbearpig1013@gmail.com>
2021-11-24 19:45:00 -0800

    created users data and encrypted with bcryptjs package

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: "Cookie",
+    email: "cookie@example.com",
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: "Nye",
+    email: "nye@example.com",
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
+
+export default users;
