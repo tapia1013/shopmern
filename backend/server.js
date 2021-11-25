8f3206db8377abeaef61d91bf9dea712d8c38780
tapia1013 <manbearpig1013@gmail.com>
2021-11-24 16:34:48 -0800

    added backend with express and some products routing

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+// ENTRY POINT FOR SERVER/BACKEND
+const express = require('express');
+const products = require('./data/products');
+
+
+const app = express();
+
+// route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+});
+
+// get single product
+app.get('/api/products/:id', (req, res) => {
+  // get url product id
+  const product = products.find(p => p._id === req.params.id);
+
+  res.json(product)
+})
+
+
+
+
+app.listen(5000, () => {
+  console.log('Server is runnning on port 5000');
+});
