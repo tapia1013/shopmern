030d16420858178adc3b5431da88776709eaa543
tapia1013 <manbearpig1013@gmail.com>
2021-11-24 19:28:03 -0800

    backend models for user,product,order

---
+++ backend/models/productModal.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+
+// review Schema for the reviews section of the productSchema
+const reviewSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
+}, { timeStamps: true });
+
+
+const productSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: 'User'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  reviews: [reviewSchema],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+}, {
+  timeStamps: true
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
