f0244fa8f71eb98bd3b6f632a554c3e1426b5e7e
tapia1013 <manbearpig1013@gmail.com>
2022-01-14 12:32:54 -0800

    productActions to homescreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,14 +4,16 @@ import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '.
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
+
     const { data } = await axios.get('/api/products');
+
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
       payload: data
     })
-  } catch (err) {
+  } catch (error) {
     dispatch({
-      type: PRODUCT_LIST_ERROR,
+      type: PRODUCT_LIST_FAIL,
       payload: error.response && error.response.data.message
         ? error.response.data.message
         : error.response
@@ -19,18 +21,3 @@ export const listProducts = () => async (dispatch) => {
   }
 }
 
-
-const fizzbuzz = (n) => {
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-      console.log('Fizz');
-    } else if (i % 5 === 0) {
-      console.log('Buzz');
-    } else {
-      console.log(i)
-    }
-  }
-}
-fizzbuzz(100)
