1bc7f4c8a751571f9ad66abda8c1f7bab1d44580
tapia1013 <manbearpig1013@gmail.com>
2022-01-14 16:15:49 -0800

    refactored the routes by creating controller folder and adding the routes logic in productController and importing it to productRoutes and using router.route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,46 +1,8 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-// bring in the MODEL
-import Product from '../models/productModel.js';
+import { getProductById, getProducts } from '../controllers/productController.js';
 
-
-
-// @desc      Fetch all products
-// @route     GET /api/products
-// @access    Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    // {} gives us everything
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-
-
-// @desc      Fetch single products
-// @route     GET /api/products/:id
-// @access    Public
-
-// get single product
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    // check if theres a product
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-
-  })
-);
-
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
