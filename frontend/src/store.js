64e8b9fad7baf555e6c9be920a32434a15f80dc9
tapia1013 <manbearpig1013@gmail.com>
2022-01-31 14:26:23 -0800

    userUpdateProfile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,7 @@ import {
   productDetailsReducer
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers';
 
 
 const reducer = combineReducers({
@@ -21,6 +21,7 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer
 });
 
 // we parse the saved cartItems from cartActions here
